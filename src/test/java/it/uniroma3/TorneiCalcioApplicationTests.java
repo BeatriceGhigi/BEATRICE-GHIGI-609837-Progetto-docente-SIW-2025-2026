757db964f2472c6eec8290e4f04fa0443db6701d
BeatriceGhigi <beaghigi44@gmail.com>
2026-04-20 14:15:54 +0200

    inizializzazione progetto

---
+++ src/test/java/it/uniroma3/TorneiCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneiCalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
